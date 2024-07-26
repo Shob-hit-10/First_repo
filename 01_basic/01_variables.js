5384f6bdf7845cd792e887b92aa624d93ae76732
Shob-hit-10 <shobhitkoli4600@gmail.com>
2024-07-26 10:20:22 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="shobhit@gmai.com"
+var accountPassword="12345"
+accountCity="Ghaziabad"
+let accountState;
+
+//accountId=2
+accountEmail="hello@gmail.com"
+var accountPassword="54321"
+accountCity="Delhi"
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
