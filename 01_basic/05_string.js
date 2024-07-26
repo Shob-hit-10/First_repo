052271156150f87090c57e2992dcafa2220c2e25
Shob-hit-10 <shobhitkoli4600@gmail.com>
2024-07-26 11:17:04 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,18 @@
+const name = "Shobhit"
+const repoCount = 50
+console.log(name + repoCount + "value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ShobhitJi')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString= gameName.slice(-8,4)
+console.log(anotherString);
+
+const url = "https://shobhit.com/shobhit%20kumar"
+console.log(url.replace('%20', '-'));
