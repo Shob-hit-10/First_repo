a18fe6a9fbe92aa31b28b415ad5446ee2e424c79
Shob-hit-10 <shobhitkoli4600@gmail.com>
2024-07-26 10:20:31 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)    We are using nodejs, not browser
+
+console.log(3+3)
+console.log("Shobhit")
+
+let name="Shobhit" 
+let age=22
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//undefined =>  
+//symbol => unique
+
+//object
+
+console.log(typeof null)    //object
+console.log(typeof undefined)    //undefined
