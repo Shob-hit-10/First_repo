1370a234fae746e1a78b8793587c926cc6655364
Shob-hit-10 <shobhitkoli4600@gmail.com>
2024-07-26 10:53:54 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -21,3 +21,29 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 let stringNumber=String(someNumber)
 console.log(typeof stringNumber);
+
+//************************* OPERATIONS *****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+
+
+let str1="helleo"
+let str2="Shobhit"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
